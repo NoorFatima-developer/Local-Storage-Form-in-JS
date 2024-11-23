8f25ea1082ec10cba860b1acd07d775bdc9b8e8c
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-24 03:59:16 +0500

    code is added

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let form = document.querySelector('form');
-let name;
+// Form ko submit krna , data ko access krna and then oss data ko local storage m dalna...
 
-form.addEventListener('submit', (event) => {
-    console.log(event);
-    event.preventDefault();     //jo b event aye osko cancel krdo, mtkb oage ko refresh krny sy rokta hai or page refresh nahi hota...
-    console.log(event);
-    console.log(event.target);
-    console.log(event.target.innerHTML);
-    console.log(event.target.uname);
-    console.log(event.target.uname.value);
-    console.log(event.target.email);
-    console.log(event.target.email.value);
-    console.log(event.target.phone);
-    console.log(event.target.phone.value);
-    
-    
-    
-      
+// Step 01--- Get Form:
+let form = document.querySelector("form");
+
+// Step 02--- Submit Form:
+form.addEventListener("submit", (event) => {
+  // Step 03--- Access Local Storage Data:
+
+  console.log(event);
+  event.preventDefault(); //jo b event aye osko cancel krdo, mtkb oage ko refresh krny sy rokta hai or page refresh nahi hota...
+  console.log(event);
+  console.log(event.target);
+  console.log(event.target.innerHTML);
+  console.log(event.target.uname);
+  console.log(event.target.uname.value);
+  console.log(event.target.email);
+  console.log(event.target.email.value);
+  console.log(event.target.phone);
+  console.log(event.target.phone.value);
 });
