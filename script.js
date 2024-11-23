6672e29072e77e1f69c334bc0d37fd7b454d324e
NoorFatima-developer <nf99noorfatima@gmail.com>
2024-11-24 03:51:53 +0500

    code is added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let form = document.querySelector('form');
+let name;
 
 form.addEventListener('submit', (event) => {
     console.log(event);
     event.preventDefault();     //jo b event aye osko cancel krdo, mtkb oage ko refresh krny sy rokta hai or page refresh nahi hota...
+    console.log(event);
+    console.log(event.target.innerHTML);
+    console.log(event.target.uname);
+    console.log(event.target.uname.value);
+      
 });
